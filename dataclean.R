f9c7096de0262a075315adf84ee92f4b5c09e986
Jstran <jstran16@student.aau.dk>
2018-10-09 11:48:45 +0200

    To foreløbige (langsomme) metoder. Begge med problemer...

--- dataclean.R
+++ dataclean.R
@@ -2,29 +2,91 @@
 
 setwd("C:/Users/jonat/Desktop/R Kode/P5/P5---Statistisk-modelering")
 
-testdata <- read.csv("p5data/TROW/TROW_20081028.csv"); tail(testdata)
+dat <- read.csv("p5data/TROW/TROW_20081028.csv")
 keepRows <- c("utcsec" , "price" , "volume")
-testdata <- testdata[keepRows] ; tail(testdata)
-# head(testdata)
-# summary(testdata)
-# str(testdata)
+dat <- dat[keepRows] ; tail(dat)
+dat$hms <- hms(dat$utcsec) ; tail(dat)
+
 library(lubridate)
 library(tidyverse)
 
-hmscol  <- hms(testdata$utcsec) ; head(hmscol)
-tcol    <-    hour(hmscol)*60 + 
-              minute(hmscol)  + 
-              second(hmscol)/60 ; head(tcol)
+
+tcol    <-    hour(dat$hms)*60   + 
+              minute(dat$hms)    + 
+              second(dat$hms)/60 ; head(tcol)
 
 pricevec <- c(0)
 n <- 1
 i <- 1
+#cldat <- data.frame(time = dat$utcsec[1] , price = dat$price[1])
+cldat <- dat[c("utcsec","price")]
+head(cldat)
+
 repeat{
   if ( ceiling(tcol[n]) < ceiling(tcol[n+1]) ){
-   pricevec[i] <- testdata$price[n]
-   i <- i +1
+    avgPlace <- n
+    avgDenom <- 1
+    avgNumerat <- dat$price[n]
+    
+    while(tcol[n] == tcol[n-1] && avgPlace > 1){
+      avgNumerat <- avgNumerat + dat$price[avgPlace]
+      avgDenom <- avgDenom + 1
+      avgPlace <- avgPlace - 1
+    }
+    if(avgDenom == 1){
+      cldat[i,1] <- dat$utcsec[n]
+      cldat[i,2] <- dat$price[n]
+      i <- i + 1
+    }
+    else{
+      cldat[i,1] <- dat$utcsec[n]
+      cldat[i,2] <- avgNumerat/avgDenom
+      i <- i + 1
+    }
   }
   n <- n + 1
-  if (n == length(tcol) ){stop("Du er nået enden")}
+  if (n == length(dat$utcsec) ){
+    cldat <- cldat[1:i-1,]
+    stop("Du er nået enden. Tillykke!")}
 }
-pricevec
+
+head(cldat)
+tail(cldat)
+cldat
+
+n <- i <- 1
+
+repeat{
+  if(minute(dat$hms[n]) < minute(dat$hms[n+1]) ){
+    avgPlace <- n
+    avgDenom <- 1
+    avgNumerat <- dat$price[n]
+    
+    while(second(dat$hms[avgPlace]) == second(dat$hms[avgPlace-1]) &&
+          avgPlace > 1) {
+      # Denne while-løkke vil nok ikke fungere hvis der kun er én
+      # observation i det første minut.
+      avgNumerat <- avgNumerat + dat$price[avgPlace]
+      avgDenom <- avgDenom + 1
+      avgPlace <- avgPlace - 1
+    }  
+    if(avgDenom == 1){
+      cldat[i,1] <- dat$utcsec[n]
+      cldat[i,2] <- dat$price[n]
+      i <- i + 1
+    }
+    else{
+      cldat[i,1] <- dat$utcsec[n]
+      cldat[i,2] <- avgNumerat/avgDenom
+      i <- i + 1
+    } 
+  }
+  n <- n + 1
+  if (n == length(dat$utcsec) ){
+    cldat <- cldat[1:i-1,]
+    stop("Du er nået enden. Tillykke!")}
+}
+head(cldat)
+tail(cldat)
+cldat
+
