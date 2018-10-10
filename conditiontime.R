9f611d79f895bce18c0e56ce2c90f5c4be8aad9f
unknown <jstran16@student.aau.dk>
2018-10-10 15:17:25 +0200

    Ligeså hurtigt at bruge lubridate hvis man laver vektorer på forhånd

--- conditiontime.R
+++ conditiontime.R
@@ -4,6 +4,7 @@ library(microbenchmark)
 
 hmscol <- hms(c("09:38:58" , "09:38:57")) ; hmscol
 tiddeci <- hour(hmscol)*60 + minute(hmscol) + second(hmscol)/60 ; tiddeci
+mincol <- minute(hmscol)
 
 microbenchmark(
   
@@ -17,6 +18,10 @@ microbenchmark(
   
   tiddeci[1]        == tiddeci[2],
   tiddeci[1]        <  tiddeci[2],
+  
+  mincol[1]         == mincol[2],
+  mincol[1]         <  mincol[2],
   unit = "ms"
 )
+# Hvis vi skal bruge minute() og sådan, skal der vektorene bare laves på forhånd. Så er evalueringerne hurtige
 
