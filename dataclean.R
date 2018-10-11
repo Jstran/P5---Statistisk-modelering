949379c6cd96e217593008a955ccae7762a9e94b
Jstran <jstran16@student.aau.dk>
2018-10-11 11:59:27 +0200

    Klar til Orimar

--- dataclean.R
+++ dataclean.R
@@ -1,12 +1,11 @@
-## Forsøg på at rense data
-setwd("C:/Users/jonat/Desktop/R Kode/P5/P5---Statistisk-modelering")
+## Attempt at cleaning data
 library(lubridate)
 library(tidyverse)
-library(microbenchmark)
 
 # Which directories to retrieve data from, and save data to
 datdir <- "./p5data"
 cldatdir <- "./p5dataclean"
+setwd("C:/Users/jonat/Desktop/R Kode/P5/P5---Statistisk-modelering")
 
 # Vector of directory paths to directories in original data folder
 dirvec <- list.dirs(datdir , full.names = 1 , recursive = 0)
@@ -21,62 +20,62 @@ for(i in 1:length(dirvec)){
 cldirvec <- list.dirs(cldatdir , full.names = 1 , recursive = 0)
 
 
-# for loop rundt om dirvec
-dirvec[1]
-j <- 1
+# Loop of all directories (stocks)
+nDir <- length(dirvec)
+for(dirloop in 1:nDir){
 
-# for loop rundt om csvvec (skal køre indeni 
-# hver iteration af løkken ovenfor)
-csvvec <- list.files(dirvec[j] , full.names = 1) ; head(csvvec)
-
-nCsv <- length(csvvec)
-
-#####
-# For loop that cleans each csv of a single stock and saves
-for (k in 1:nCsv){
-
-dat <- read.csv(csvvec[k])
-keepRows <- c("utcsec" , "price")
-dat <- dat[keepRows] ; tail(dat)
-dat$hms <- hms(dat$utcsec) ; tail(dat)
-
-# Time vector in minutes
-tcol    <-    hour(dat$hms)*60   + 
-              minute(dat$hms)    + 
-              second(dat$hms)/60 ; head(tcol)
-
-cldat <- dat[c("utcsec","price")] ; head(cldat)
-floorvec <- floor(tcol)
-secvec   <- second(dat$hms)
-last <- length(dat$utcsec)
-n <- i <- 1
-
-repeat{
-    if(floorvec[n] < floorvec[n+1] ){
-      avgPlace <- n
-      while(secvec[avgPlace] == secvec[avgPlace-1] &&
-            avgPlace > 1) {
-        avgPlace <- avgPlace - 1
-      }  
-      if(avgPlace == n){
-        cldat[i,1] <- dat$utcsec[n]
-        cldat[i,2] <- dat$price[n]
-      }
-      else{
-        cldat[i,1] <- dat$utcsec[n]
-        cldat[i,2] <- sum(dat$price[avgPlace:n])/(n-avgPlace+1)
-      }
-      i <- i + 1
+  # Vector of file names for stock in the current iteration
+  csvvec <- list.files(dirvec[dirloop] , full.names = 1)
+  
+  # For loop that cleans each csv of a single stock and saves
+  nCsv <- length(csvvec)
+  for (csvloop in 1:nCsv){
+  
+    dat <- read.csv(csvvec[csvloop])
+    keepRows <- c("utcsec" , "price")
+    dat <- dat[keepRows] ; tail(dat)
+    dat$hms <- hms(dat$utcsec) ; tail(dat)
+    
+    # Time vector in minutes
+    tcol    <-    hour(dat$hms)*60   + 
+                  minute(dat$hms)    + 
+                  second(dat$hms)/60 ; head(tcol)
+    
+    cldat <- dat[c("utcsec","price")] ; head(cldat)
+    floorvec <- floor(tcol)
+    secvec   <- second(dat$hms)
+    last <- length(dat$utcsec)
+    n <- i <- 1
+    
+    # Cleaning loop
+    repeat{
+        if(floorvec[n] < floorvec[n+1] ){
+          avgPlace <- n
+          while(secvec[avgPlace] == secvec[avgPlace-1] &&
+                avgPlace > 1) {
+            avgPlace <- avgPlace - 1
+          }  
+          if(avgPlace == n){
+            cldat[i,1] <- dat$utcsec[n]
+            cldat[i,2] <- dat$price[n]
+          }
+          else{
+            cldat[i,1] <- dat$utcsec[n]
+            cldat[i,2] <- sum(dat$price[avgPlace:n])/(n-avgPlace+1)
+          }
+          i <- i + 1
+        }
+        n <- n + 1
+        if (n == last ){
+          cldat[i,1] <- dat$utcsec[n]
+          cldat[i,2] <- dat$price[n]
+          cldat <- cldat[1:i,]
+          break}
     }
-    n <- n + 1
-    if (n == last ){
-      cldat[i,1] <- dat$utcsec[n]
-      cldat[i,2] <- dat$price[n]
-      cldat <- cldat[1:i,]
-      stop("Du er nået enden. Tillykke!") }
-}
-
-
-# Gemmer filen
-#write.csv(cldat , file = file.path(cldirvec[j] , basename(csvvec[k])) )
+    
+    
+    # Save clean file to new directory
+    write.csv(cldat , file = file.path(cldirvec[dirloop] , 
+                                       basename(csvvec[csvloop])) )
+  }
 }
