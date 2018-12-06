8b1b0d2aacd15487001057c6bdd00e935385fde5
Jstran <jstran16@student.aau.dk>
2018-12-06 09:51:51 +0100

    .

--- betaandpframe.R
+++ betaandpframe.R
@@ -91,14 +91,16 @@ factdir <- "./factors"
 factcsv <- list.files(factdir , full.names = 1 , recursive = 0)
 
 # HML
-dat <- read.xlsx(factcsv[1])
+dat <- read.xlsx(factcsv[2])
 HML <- data.frame(date = ymd(dat$X1) , return = log(1 + dat$X3))
+#HML <- data.frame(date = ymd(dat$X1) , return = dat$X3 )
 HML <- HML[1:218514,]
 HML$return[1] <- 0
   
 # SMB
-dat <- read.xlsx(factcsv[2])
+dat <- read.xlsx(factcsv[3])
 SMB <- data.frame(date = ymd(dat$X1) , return = log(1 + dat$X3))
+#SMB <- data.frame(date = ymd(dat$X1) , return = dat$X3 )
 SMB <- SMB[1:218514,]
 SMB$return[1] <- 0
 
@@ -192,7 +194,7 @@ insignHML <- snames[pframe$pHML > 0.05] ; insignHML
 # Kør først (1) og (2)
 
 # Laver naiv portefølje data.frame
-allstock <- data.frame(numeric(218435))
+allstock <- data.frame(numeric(218434))
 j <- 1
 datevec <- c()
 
@@ -201,23 +203,23 @@ for(k in (1:36)[!1:36 %in% 28]){ # Aktier uden SPY
   pricevec <- c()
   
   for(i in 1:(length(dat) - 1) ){
-    pricevec <- c(pricevec,dat[priceseq,(i+1)])
+    pricevec <- c(pricevec , dat[priceseq , (i+1)] )
   }
   if(k == 2){ # Korteste aktie
     for(h in 1:(length(dat) - 1)){
-      datevec  <- c(datevec , rep(names(dat)[(h+1)],(391 + n - 1)/n) )  
+      datevec  <- c(datevec , rep(names(dat)[(h+1)],(391 + n - 1)/n))  
       
     }
     datevec <- ymd(substring(datevec,2))
   }
   
-  allstock[,j] <- pricevec[1:218435]
+  allstock[,j] <- diff(log(pricevec))[1:218434] # FEJL
   names(allstock)[j] <- snames[k]
   j <- j + 1
   print(k)
 }
 naiveport <- data.frame(date = datevec[1:218434] ,
-                        return = diff(log(rowSums(allstock))))
+                        return = (1/35) * rowSums(allstock) ) # FEJL
 
 # Undersøger lineær model baseret på naiv portefølje 
 betaSMB <- c()
@@ -305,13 +307,14 @@ for(y in 1999:2009){
                                           SMB$return[int])
     rcapm <- c(rcapm , summary(capmmod)$adj.r.squared)
     rfama <- c(rfama , summary(famamod)$adj.r.squared)
-    rdate <- c(rdate , (naiveport$date[int])[1] )
+    rdate <- c(rdate , (naiveport$date[int])[1] ) # VIRKER IKKE FUCK R
   }
 }
 
+
 plot(rfama - rcapm , type = "l" , 
-                     ylim = c(-0.01 , 0.03) ,
                      col = "blue" ,
+                     ylim = c(-0.01 , 0.03) ,
                      xlab = "Måneder efter 1999-01" ,
                      ylab = "R^2 Differens (Fama - CAPM)")
 abline(h = 0)
@@ -319,16 +322,62 @@ abline(h = 0)
 ### (11) TEST -------------------------------------------------------
 betaSMB <- c()
 beta0 <- c()
-for(y in 1999:2009){
-  for(m in 1:12){
+for(y in 2004){
+  for(m in 8){
     int <- year(stock$date) == y & month(stock$date) == m 
+    k <- length(stock$return[int])
+    ds <- length( unique( day( stock$date[int] )  ) )
+    h <- 1/(391*ds) * rep(1,k) # 1/(antal obs på måneden (391 * dage)) 
     famamod <- lm( stock$return[int] ~ SPY$return[int] + 
                                        HML$return[int] + 
                                        SMB$return[int])
     
-    betaSMB <- c(betaSMB , as.numeric(mymodel$coefficients[4]) )
+    betaSMB <- c(betaSMB , as.numeric(famamod$coefficients[4]) )
     beta0 <- c(beta0 , summary(famamod)$coefficients[1,4])
   }
 }
+
+
+betaSMB <- c()
+beta0 <- c()
+
+pframe <- data.frame(pbeta0  = rep(10,132) ,
+                     pSPY    = rep(10,132) ,
+                     pHML    = rep(10,132) ,
+                     pSMB    = rep(10,132)) ; head(pframe)
+ppos <- 1
+for(y in 2004){
+  for(m in 8){
+    int <- year(naiveport$date) == y & month(naiveport$date) == m
+    int <- c(int , rep(FALSE , 80)) # Fordi porteføljen er kortere
+    
+    k <- length(naiveport$return[int])
+    ds <- length( unique( day( naiveport$date[int] )  ) )
+    h <- 1/(79*ds) * rep(1,k) # 1/(antal obs på måneden (391 * dage)) 
+    
+    famamod <- lm( naiveport$return[int] ~ h               +
+                                           SPY$return[int] + 
+                                           HML$return[int] + 
+                                           SMB$return[int] -
+                                           1)
+    
+    #betaSMB <- c(betaSMB , as.numeric(famamod$coefficients[4]) )
+    #beta0 <- c(beta0 , summary(famamod)$coefficients[1,4])
+    
+    pframe[ppos,] <- as.numeric(summary(famamod)$coefficients[,4])
+    ppos <- ppos + 1
+  }
+  print(y)
+}
+
+signbeta0 <- pframe$pbeta0[pframe$pbeta0 < 0.05] ; length(signbeta0)
+insignSPY <- pframe$pSPY[pframe$pSPY > 0.05] ; length(insignSPY)
+insignSMB <- pframe$pSMB[pframe$pSMB > 0.05] ; length(insignSMB)
+insignHML <- pframe$pHML[pframe$pHML > 0.05] ; length(insignHML)
+insignALL <- pframe$pSPY[pframe$pSPY > 0.05 &
+                         pframe$pSMB > 0.05 &
+                         pframe$pHML > 0.05] ; length(insignALL)
+summary(famamod)
+plot(famamod$residuals , pch = 20 , cex = 0.7 , col = "blue")
 
 
